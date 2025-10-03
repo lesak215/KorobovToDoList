1929e373b0d525bc4daa5392fe86bbf6bbe588a6
lesak215 <lesak215@gmail.com>
2025-10-03 12:53:24 +1000

    Добавлен класс для информационных сообщений

---
+++ KorobovToDoList/classes/MessageBoxInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KorobovToDoList.classes
+{
+    public class MessageBoxInfo
+    {
+        public static void ShowSuccess(string message, string title = "Успех!")
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        public static void ShowError(string message, string title = "Ошибка!")
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        public static void ShowWarning(string message, string title = "Внимание!")
+        {
+            MessageBox.Show(message, title, MessageBoxButton.YesNo, MessageBoxImage.Warning);
+        }
+    }
+}
